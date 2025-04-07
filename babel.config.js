221fe33c78a7f39033f4863657f3382f3b7b4235
Егор Купрацевич <kupratsevich.egor@gmail.com>
2025-04-07 18:37:00 +0300

    feat: architecture and store

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,29 @@
 module.exports = {
-  presets: ['module:@react-native/babel-preset'],
+  presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        alias: {
+          '@app': './src/app',
+          '@shared': './src/shared',
+          '@entities': './src/entities',
+          '@features': './src/features',
+          '@widgets': './src/widgets',
+          '@pages': './src/pages',
+        },
+        extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
+      },
+    ],
+    [
+      'module:react-native-dotenv',
+      {
+        moduleName: '@env',
+        path: '.env',
+        safe: false,
+        allowUndefined: true,
+      },
+    ],
+  ],
 };
---
+++ src/shared/const/env.ts
@@ -0,0 +1,3 @@
+import {API_URL as BASE_API_URL } from '@env';
+
+export const API_URL = BASE_API_URL;
